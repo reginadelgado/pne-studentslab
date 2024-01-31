5771076977b5f7342e7f4b1a208ebbd8b2bf02a1
reginada <r.delgadoa.2023@alumnos.urjc.es>
2024-01-31 18:29:27 +0100

    session 3

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -3,7 +3,6 @@ with open("dna.txt", "r") as f:
     for line in f:
         sequence += line.replace("\n", "")
 def dna_count(seq):
-    print("Sequence:", seq)
     dictionary = {"A": 0, "C": 0, "T": 0, "G": 0}
     for e in seq:
         dictionary[e] += 1
