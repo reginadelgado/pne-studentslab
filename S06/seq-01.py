2eb3e81e3327f4d8600492fbb45855bb03b158c6
reginadelgado <r.delgadoa.2023@alumnos.urjc>
2024-02-13 21:14:45 +0100

    S5 P00

--- S06/seq-01.py
+++ S06/seq-01.py
@@ -21,14 +21,24 @@ class Gene(Seq):
        All the objects of class Gene will inherit
        the methods from the Seq class
     """
-    pass
+    def __init__(self, strbases, name=""):
+
+        # -- Call first the Seq initializer and then the
+        # -- Gene init method
+        super().__init__(strbases)
+        self.name = name
+        print("New gene created")
+
+    def __str__(self):
+        """Print the Gene name along with the sequence"""
+        return self.name + "-" + self.strbases
+
 
 # --- Main program
 s1 = Seq("AGTACACTGGT")
-g = Gene("CGTAAC")
+g = Gene("CGTAAC", "FRAT1")
 
 # -- Printing the objects
 print(f"Sequence 1: {s1}")
-print(f"  Length: {s1.len()}")
 print(f"Gene: {g}")
-print(f"  Length: {g.len()}")
+
