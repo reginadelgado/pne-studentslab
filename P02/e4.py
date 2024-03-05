ac73810ea769ffcef4fbab4b6622b6d747cc33ca
reginadelgado <r.delgadoa.2023@alumnos.urjc>
2024-03-05 12:07:31 +0100

    S09 P02

--- P02/e4.py
+++ P02/e4.py
@@ -1,2 +1,31 @@
 from Seq1 import Seq
+from Client0 import Client
+from termcolor import *
 
+#IP uni: 212.128.255.82
+#IP casa: 192.168.1.46
+
+IP = "192.168.1.46" # your IP address
+PORT = 8081
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+# -- Test the ping method
+print(c)
+
+gene_list = ["U5", "FRAT1", "ADA"]
+
+for name in gene_list:
+    msg = f"Sending {name} Gene to the server..."
+    print(f"To Server: {colored(msg, 'blue')}")
+    response = c.talk(msg)
+    print(f"From Server\n\n{colored(response, 'yellow')}")
+
+    gene = Seq()
+    filename = name + ".txt"
+    gene.read_fasta(filename)
+
+    print(f"To Server: {colored(str(gene), 'blue')}")
+    response2 = c.talk(str(gene))
+    print(f"From Server\n\n{colored(response2, 'yellow')}")
--- P02/e6.py
+++ P02/e6.py
@@ -5,24 +5,34 @@ from Client0 import Client
 #IP casa: 192.168.1.46
 
 IP = "192.168.1.46" # your IP address
-PORT = 8081
+PORT = 8080
+PORT2 = 8081
+
 
 # -- Create a client object
-c = Client(IP, PORT)
+c1 = Client(IP, PORT)
+c2 = Client(IP, PORT2)
 
 # -- Test the ping method
-print(c)
+print(c1)
+print(c2)
 
 s = Seq()
 s.read_fasta("FRAT1.txt")
 seq = str(s)
 print(f"Gene FRAT1: {s}")
-c.talk("Sending FRAT1 Gene to the server, in fragments of 10 bases...")
+
+c1.talk("Sending FRAT1 Gene to the server, in fragments of 10 bases...")
+c2.talk("Sending FRAT1 Gene to the server, in fragments of 10 bases...")
+
 n1, n2 = 0, 10
-for i in range(5):
+for i in range(10):
     fragment = seq[n1:n2]
     msg = f"Fragment {i + 1}: {fragment}"
     print(msg)
-    c.talk(msg)
+    if i % 2 == 0:
+        c1.talk(msg)
+    else:
+        c2.talk(msg)
     n1 += 10
     n2 += 10
