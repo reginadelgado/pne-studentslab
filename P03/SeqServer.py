839adb8fdee0000c6301ff77fbcfdbe5a423c192
reginadelgado <r.delgadoa.2023@alumnos.urjc>
2024-03-10 18:17:36 +0100

    S11 P03

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -4,7 +4,7 @@ from termcolor import *
 
 # Configure the Server's IP and PORT
 PORT = 8080
-IP = "127.0.0.1" # the IP address depends on the machine running the server
+IP = "127.0.0.1"  # the IP address depends on the machine running the server
 
 # -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,11 +18,18 @@ ls.bind((IP, PORT))
 # -- Step 3: Configure the socket for listening
 ls.listen()
 
-print("The server is configured!")
+print("SEQ Server is configured!")
+s1 = "ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA"
+s2 = "AAAAACATTAATCTGTGGCCTTTCTTTGCCATTTCCAACTCTGCCACCTCCATCGAACGA"
+s3 = "CAAGGTCCCCTTCTTCCTTTCCATTCCCGTCAGCTTCATTTCCCTAATCTCCGTACAAAT"
+s4 = "CCCTAGCCTGACTCCCTTTCCTTTCCATCCTCACCAGACGCCCGCATGCCGGACCTCAAA"
+s5 = "AGCGCAAACGCTAAAAACCGGTTGAGTTGACGCACGGAGAGAAGGGGTGTGTGGGTGGGT"
+
+seq_list = [s1, s2, s3, s4, s5]
 
 while True:
     # -- Waits for a client to connect
-    print("Waiting for Clients to connect")
+    print("Waiting for Clients ...")
 
     try:
         (cs, client_ip_port) = ls.accept()
@@ -31,7 +38,7 @@ while True:
     except KeyboardInterrupt:
         print("Server stopped by the user")
 
-        # -- Close the listenning socket
+        # -- Close the listening socket
         ls.close()
 
         # -- Exit!
@@ -40,7 +47,7 @@ while True:
     # -- Execute this part if there are no errors
     else:
 
-        print("A client has connected to the server!")
+        # print("A client has connected to the server!")
 
         # -- Read the message from the client
         # -- The received message is in raw bytes
@@ -49,12 +56,57 @@ while True:
         # -- We decode it for converting it
         # -- into a human-redeable string
         msg = msg_raw.decode()
+        msg_l = msg.split(" ")
 
-        # -- Print the received message
-        print(f"Message received: {colored(msg, 'green')}")
+        command = msg_l[0]
+        print(f"{colored(command, 'yellow')}")
 
+        if command == "PING" and len(msg_l) == 1:
+            response = "OK!"
+            # command = "PING command!"
+
+        elif len(msg_l) == 2:
+            command = msg_l[0]
+            if command == "GET":
+                n_seq = msg_l[1]
+                if n_seq.isdigit() and 0 <= int(n_seq) <= 4:
+                    response = seq_list[int(n_seq)]
+
+                else:
+                    response = "Not valid number"
+
+            elif command == "INFO":
+                seq = Seq(msg_l[1])
+                response = f"Sequence: {seq}\nTotal length: {seq.len()}\n"
+                for i in ["A", "C", "G", "T"]:
+                    response += f"{i}: {seq.count_base(i)} ({seq.percentage(i)})\n"
+
+            elif command == "COMP":
+                seq = Seq(msg_l[1])
+                response = seq.complement()
+
+            elif command == "REV":
+                seq = Seq(msg_l[1])
+                response = seq.reverse()
+
+            elif command == "GENE":
+                gene_name = msg_l[1]
+                if gene_name in ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]:
+                    filename = gene_name + ".txt"
+                    seq = Seq()
+                    seq.read_fasta(filename)
+                    response = str(seq)
+                else:
+                    response = "Not valid Gene"
+
+            else:
+                response = "Unknown command"
+        else:
+            response = "Unknown command"
+
+        print(response, "\n")
         # -- Send a response message to the client
-        response = f"ECHO: {msg}"
+        # response = f"ECHO: {msg}"
 
         # -- The message has to be encoded into bytes
         cs.send(response.encode())
---
+++ P03/test-client.py
@@ -0,0 +1,44 @@
+from Client0 import Client
+
+print(f"-----| Practice 3, Exercise 7 |------")
+
+PORT = 8080
+IP = "127.0.0.1"
+
+c = Client(IP, PORT)
+
+print(c)
+
+print("* Testing PING")
+response = c.talk("PING")
+print(response)
+
+print("* Testing GET")
+for i in range(5):
+    response = c.talk(f"GET {i}")
+    print(f"GET {i}: {response}")
+
+seq = "ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA"
+
+print("\n* Testing INFO")
+response = c.talk(f"INFO {seq}")
+print(response)
+
+print("* Testing COMP")
+response = c.talk(f"COMP {seq}")
+print(f"SEQ: {seq}")
+print(f"COMP: {response}")
+
+print("\n* Testing REV")
+response = c.talk(f"COMP {seq}")
+print(f"SEQ: {seq}")
+print(f"COMP: {response}")
+
+gene_list = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
+
+print("\n* Testing GENE")
+for gene in gene_list:
+    msg = f"GENE {gene}"
+    response = c.talk(msg)
+    print(msg)
+    print(response + "\n")
