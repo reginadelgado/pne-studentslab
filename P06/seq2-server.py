5aae1a5a7382b9696278d0ff7b2d9943b8a5452f
reginadelgado <r.delgadoa.2023@alumnos.urjc>
2024-04-21 19:22:31 +0200

    P06

--- P06/seq2-server.py
+++ P06/seq2-server.py
@@ -4,6 +4,7 @@ import termcolor
 from pathlib import Path
 from urllib.parse import parse_qs, urlparse
 import jinja2 as j
+from Seq1 import Seq
 
 # Define the Server's port
 PORT = 8080
@@ -12,7 +13,7 @@ PORT = 8080
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
 def read_html_file(filename):
-    contents = Path("html/" + filename).read_text()
+    contents = Path("html" + filename).read_text()
     contents = j.Template(contents)
     return contents
 
@@ -38,28 +39,41 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             # Generating the response message
             self.send_response(200)  # -- Status line: OK!
 
-        elif path == "/ping":
-            contents = Path('html/ping.html').read_text()
-            self.send_response(200)
-
-        elif path == "/get":
-            s1 = "ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA"
-            s2 = "AAAAACATTAATCTGTGGCCTTTCTTTGCCATTTCCAACTCTGCCACCTCCATCGAACGA"
-            s3 = "CAAGGTCCCCTTCTTCCTTTCCATTCCCGTCAGCTTCATTTCCCTAATCTCCGTACAAAT"
-            s4 = "CCCTAGCCTGACTCCCTTTCCTTTCCATCCTCACCAGACGCCCGCATGCCGGACCTCAAA"
-            s5 = "AGCGCAAACGCTAAAAACCGGTTGAGTTGACGCACGGAGAGAAGGGGTGTGTGGGTGGGT"
-
-            seq_list = [s1, s2, s3, s4, s5]
-            n = arguments.get("n")[0]
-            seq = seq_list[int(n)]
-
-            contents = read_html_file('html/get.html').render(context={"todisplay": seq})
-
-            self.send_response(200)
-
         else:
-            contents = Path("html/error.html").read_text()
-            self.send_response(404)
+            try:
+                contents = read_html_file(path + ".html")
+                if path == "/ping":
+                    pass
+
+                elif path == "/get":
+                    s1 = "ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA"
+                    s2 = "AAAAACATTAATCTGTGGCCTTTCTTTGCCATTTCCAACTCTGCCACCTCCATCGAACGA"
+                    s3 = "CAAGGTCCCCTTCTTCCTTTCCATTCCCGTCAGCTTCATTTCCCTAATCTCCGTACAAAT"
+                    s4 = "CCCTAGCCTGACTCCCTTTCCTTTCCATCCTCACCAGACGCCCGCATGCCGGACCTCAAA"
+                    s5 = "AGCGCAAACGCTAAAAACCGGTTGAGTTGACGCACGGAGAGAAGGGGTGTGTGGGTGGGT"
+
+                    seq_list = [s1, s2, s3, s4, s5]
+                    n = arguments.get("n")[0]
+                    seq = seq_list[int(n)]
+
+                    contents = contents.render(context={"seq": seq, "n": n})
+
+                elif path == "/gene":
+                    name = arguments.get("name")[0]
+                    filename = name + ".txt"
+                    seq = Seq()
+                    seq.read_fasta(filename)
+
+                    contents = contents.render(context={"gene": seq, "name": name})
+
+                elif path == "/operation":
+                    seq = arguments.get("seq",[""])[0]
+
+                self.send_response(200)
+
+            except (FileNotFoundError, TypeError, IndexError):
+                contents = Path("html/error.html").read_text()
+                self.send_response(404)
 
         # Define the content-type header:
         self.send_header('Content-Type', 'text/html')
