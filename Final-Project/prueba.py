4ebeebb8ced0086f8279ba9c33d67a36416b8cbd
reginada <r.delgadoa.2023@alumnos.urjc.es>
2024-05-06 16:36:24 +0200

    Final Project

---
+++ Final-Project/prueba.py
@@ -0,0 +1,45 @@
+import http.client
+import json
+
+
+SERVER = "rest.ensembl.org"
+PARAMS = "?content-type=application/json"
+ENDPOINT = "/info/species"
+
+URL = SERVER + ENDPOINT + PARAMS
+
+print(f"Server: {SERVER}")
+print(f"URL: {URL}")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", ENDPOINT + PARAMS)
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+# -- Transform it into JSON format
+response = json.loads(data1)
+
+all_species = response["species"]
+
+names = ""
+
+for i in range(40):
+    s = all_species[i]
+    n = s["display_name"]
+    names += f"-{n}\n"
+print(names)
---
+++ Final-Project/server.py
@@ -0,0 +1,119 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+from urllib.parse import parse_qs, urlparse
+import jinja2 as j
+from Seq1 import Seq
+
+# Define the Server's port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+def read_html_file(filename):
+    contents = Path(filename).read_text()
+    contents = j.Template(contents)
+    return contents
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inherits all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        url_path = urlparse(self.path)
+        path = url_path.path
+        arguments = parse_qs(url_path.query)
+
+        if path == "/":
+            # Open the form1.html file
+            contents = Path('main_page.html').read_text()
+
+            # Generating the response message
+            self.send_response(200)  # -- Status line: OK!
+
+        else:
+            try:
+                contents = read_html_file(path + ".html")
+                if path == "/ping":
+                    contents = Path('html/ping.html').read_text()
+
+                elif path == "/listSpecies":
+
+                    seq_list = [s1, s2, s3, s4, s5]
+                    n = arguments.get("n")[0]
+                    seq = seq_list[int(n)]
+
+                    contents = contents.render(context={"seq": seq, "n": n})
+
+                elif path == "/gene":
+                    name = arguments.get("name")[0]
+                    filename = name + ".txt"
+                    seq = Seq()
+                    seq.read_fasta(filename)
+
+                    contents = contents.render(context={"gene": seq, "name": name})
+
+                elif path == "/operation":
+                    seq = arguments.get("seq", [""])[0]
+                    operation = arguments.get("operation", [""])[0]
+                    seq = Seq(seq)
+
+                    if operation == "Info":
+                        result = f"Total length: {seq.len()}"
+                        for i in ["A", "C", "G", "T"]:
+                            result += f"<br>{i}: {seq.count_base(i)} ({seq.percentage(i)})"
+
+                    elif operation == "Comp":
+                        result = seq.complement()
+
+                    elif operation == "Rev":
+                        result = seq.reverse()
+
+                    contents = contents.render(context={"seq": seq, "operation": operation, "result": result})
+                self.send_response(200)
+
+            except (FileNotFoundError, TypeError, IndexError):
+                contents = Path("error.html").read_text()
+                self.send_response(404)
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
